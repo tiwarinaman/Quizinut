ddc7516fa02a8cfd98169df7ac8ff6f321ceacb9
Naman Kumar <namant744@gmail.com>
2021-04-23 16:54:20 +0530

    setting changed

--- Quizinut/settings.py
+++ Quizinut/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)e1tt4gb$vi1qy8jq3(jnb*n+c57t$p(ywhprtgnx38g)e=&kg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
